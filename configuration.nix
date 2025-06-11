fd0032bcb709d4884d155e179f3ffac6142069d6
Claudius Galo <galoclaudius@icloud.com>
2025-06-11 02:13:59 -0500

    Auto-commit after nixos-rebuild: 2025-06-11 02:13:59

--- configuration.nix
+++ configuration.nix
@@ -99,6 +99,12 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  # Enable remote desktop
+    services.xrdp = {
+    enable = true;
+    defaultWindowManager = "gnome";  # or "plasma" / "gnome" / "xterm"
+    };
+
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
@@ -191,7 +197,7 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
+  
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -199,5 +205,5 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
-
+   
 }
